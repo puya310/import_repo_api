b8a6fde7175f0425e45ff5f157ff484465d5e935
puya310 <puya310r@gmail.com>
2023-09-21 11:04:25 -0400

    push

---
+++ import_repo.py
@@ -0,0 +1,55 @@
+import requests
+import time
+
+SNYK_TOKEN = "" #your snyk API token (account settings on snyk UI)
+ORG_ID = "" #Org ID (this will need to be done/run for each org)
+integrationId = "" #SCM integrationID (get from integration page)
+branch = "master" #set the default branch to monitor (main/master, which is what Snyk monitors by default)
+
+#takes displayName attribute from target_url endpoint and feeds it into the import_url api
+#considerations: branch 
+target_url = f"https://api.snyk.io/rest/orgs/{ORG_ID}/targets?version=2023-09-14%7Ebeta&origin=azure-repos"
+import_url = f"https://api.snyk.io/v1/org/{ORG_ID}/integrations/{integrationId}/import"
+
+all_targets = []
+
+headers = {
+    'Accept': 'application/vnd.api+json',
+    'Authorization': f'{SNYK_TOKEN}'
+    }
+res = requests.request("GET", target_url, headers=headers).json()
+all_targets.extend(res['data'])
+
+    
+for project in all_targets:
+    # for each all_targets, get the displayName (format is usually repo/name) and split it to pass into the import API
+    display_name=(project['attributes']['displayName'])
+    splitName = display_name.split('/')
+    if len(splitName) >= 2:
+        owner = splitName[1] 
+        name = splitName[0]
+    else:
+        print("invalid / format for the repo ")
+
+    request_data= { #body to post 
+        'target': {
+         'owner': owner,
+         'name': name,
+         'branch': branch
+        }
+    }
+    headers = { 
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization': f'{SNYK_TOKEN}'
+    }
+
+    response = requests.post(import_url, headers=headers, json=request_data)
+    time.sleep(3)
+
+    if response.status_code == 200 or 201:
+        print("Request was successful. Importing " + name)
+    # Process the API response data if needed
+        response_data = response.json()
+        print("Response Data:", response_data)
+    else:
+        print(f"Request failed with status code: {response.status_code}")
